7a47f71819ceffe65b435d774e41e1d48e15a8f9
Giuliano Sampaio <giuguitar@gmail.com>
2023-07-09 11:57:13 -0300

    fix user seed

--- Console/SetupCommand.php
+++ Console/SetupCommand.php
@@ -38,16 +38,44 @@ class SetupCommand extends Command
      */
     public function handle()
     {
+        $this->setTheme()
+             ->enableModules()
+             ->migrateDatabase()
+             ->seedModules(); 
+        
+        $this->info('Admin module configurated with successful');      
+    }
+
+    private function setTheme() : self
+    {
+        Artisan::call('samurai:publish maestriam/stylus');
+        Artisan::call('samurai:use maestriam/stylus');
+        Artisan::call('samurai:refresh');  
+
+        return $this;
+    }
+
+    private function enableModules() : self
+    {
         Artisan::call('module:enable Admin');
         Artisan::call('module:enable Users');
         Artisan::call('module:enable Accounts');
+
+        return $this;
+    }
+
+    private function migrateDatabase() : self
+    {        
         Artisan::call('maestro:migrate Accounts');
         Artisan::call('maestro:migrate Users');
-        Artisan::call('samurai:publish maestriam/stylus');
-        Artisan::call('samurai:use maestriam/stylus');
-        Artisan::call('samurai:refresh');   
-        sleep(1);     
+
+        return $this;
+    }
+
+    private function seedModules() : self
+    {
         Artisan::call('maestro:seed Users');
-        $this->info('Admin module configurated with succesful');      
+
+        return $this;
     }
 }
