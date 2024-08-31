d84e16cc98d5d567e19eadd9f42bf1761a3e39b9
Giuliano Sampaio <giuguitar@gmail.com>
2024-08-31 18:02:48 -0300

    edit empty-state

--- Resources/views/components/empty-state.blade.php
+++ Resources/views/components/empty-state.blade.php
@@ -1,4 +1,7 @@
-<div class="empty-state" >
+<div class="empty-state">
+    <div class="empty-state-icon {{ $color ?? '' }}">
+        <i class="fas {{ $icon ?? 'fa-question' }}"></i>
+    </div>
     <h2>{{ $title ?? __('admin::empty-state.title') }}</h2>
     <p class="lead">{{ $description ?? __('admin::empty-state.description') }}</p> 
 </div>
