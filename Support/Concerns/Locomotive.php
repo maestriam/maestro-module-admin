b6f1fe69bdd5e45cec289cc17012afc5b092a631
Giuliano Sampaio <giuguitar@gmail.com>
2025-01-29 18:16:12 -0300

    improving locomotive

--- Support/Concerns/Locomotive.php
+++ Support/Concerns/Locomotive.php
@@ -7,32 +7,59 @@ use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Event;
 
 /**
- * Orquestrador de Jobs/Eventos via laravel
+ * Orquestrador de Jobs/Eventos via laravel.
+ * @todo Verificar a possibilidade desse trait ser apenas
+ * uma função chamada locomotive(), onde retorna uma instância
+ * para uma classe onde retorna as regras de negócio. 
+ * 
+ * ex: locomotive()->push('minha-fila', MeuJob::class);
  */
 trait Locomotive
 {
-    public function push(string $name, string $job) : void
+    /**
+     * Insere um novo job para uma determinada fila.
+     *
+     * @param string $queue
+     * @param string $job
+     * @return void
+     */
+    public function push(string $queue, string $job) : void
     {
-        $jobs = $this->jobs($name, $job);
+        $jobs = $this->jobs($queue, $job);
 
         if ($jobs == null) return;
 
         $jobs->push($job);
 
-        Cache::put($name, $jobs);
+        Cache::put($queue, $jobs);
     }
 
-    public function first(string $name, string $job)
+    /**
+     * Insere um novo serviço em uma fila no ínicio como primeiro
+     * item da fila. 
+     *
+     * @param string $queue
+     * @param string $job
+     * @return void
+     */
+    public function first(string $queue, string $job)
     {
-        $jobs = $this->jobs($name, $job);
+        $jobs = $this->jobs($queue, $job);
 
         if ($jobs == null) return;
 
         $jobs->prepend($job);
 
-        Cache::put($name, $jobs);
+        Cache::put($queue, $jobs);
     }
 
+    /**
+     * Insere um novo job no final da fila. 
+     *
+     * @param string $name
+     * @param string $job
+     * @return void
+     */
     public function last(string $name, string $job)
     {
         $jobs = $this->jobs($name, $job);
@@ -44,23 +71,48 @@ trait Locomotive
         Cache::put($name, $jobs);
     }
 
-    public function event(string $event, string $name) 
+    public function event(string $queue, mixed $params) 
     {
-        $jobs = Cache::get($name, collect([]))->toArray();
+        $jobs = Cache::get($queue, collect([]))->toArray();
 
         if (empty($jobs)) return;
-        
-        Event::listen($event, function($v) use ($jobs) {
-            try {
-                foreach($jobs as $job) {
-                    app($job)->handle($v);
-                }
-            } catch (\Throwable $th) {
-                return $th;
-            }
-        });
-    }    
 
+        $this->runJobs($jobs, $params);
+    }
+
+    /**
+     * Undocumented function
+     *
+     * @return void
+     */
+    private function runJobs(array $jobs, mixed $params)
+    {
+        foreach($jobs as $job) {
+
+            $listener = app($job);
+
+            $listener->handle($params);
+        }        
+    }
+
+    /**
+     * Limpa os jobs de uma determinada fila. 
+     *
+     * @param string $queue
+     * @return void
+     */
+    public function clear(string $queue)
+    {
+        return Cache::forget($queue);
+    }
+
+    /**
+     * Undocumented function
+     *
+     * @param string $name
+     * @param string $job
+     * @return Collection|null
+     */
     private function jobs(string $name, string $job) : ?Collection
     {
         $jobs = Cache::get($name, collect([]));                
