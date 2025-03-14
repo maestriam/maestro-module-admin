61493af6c7aa95a8824a9bd3deab170547e43769
Giuliano Sampaio <giuguitar@gmail.com>
2025-03-14 04:51:59 -0300

    Ymd function bugfix

--- Support/Helpers/Ymd.php
+++ Support/Helpers/Ymd.php
@@ -2,8 +2,21 @@
 
 use Illuminate\Support\Carbon;
 
-function Ymd(string $date) 
+/**
+ * Retorna uma data do formato americano (d/m/Y H:i) 
+ * para o fomato europeu (Y-m-d H:i).   
+ * Caso a data passada não esteja no formato americano, 
+ * a função retorna o próprio valor passado.  
+ *
+ * @param string $date
+ * @return void
+ */
+function Ymd(string $date) : string
 {
+    $check = '/^\d{2}\/\d{2}\/\d{4} \d{2}:\d{2}$/';
+
+    if (! preg_match($check, $date)) return $date; 
+
     $pattern = 'd/m/Y H:i';
     $convert = 'Y-m-d H:i';
     
